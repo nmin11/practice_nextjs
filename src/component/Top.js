08e14c78513938535c7500f58de5954c3354821f
nmin11 <nmin1124@gmail.com>
2021-10-20 22:36:43 +0900

    initial commit

---
+++ src/component/Top.js
@@ -0,0 +1,16 @@
+import { Header } from 'semantic-ui-react';
+import Gnb from './Gnb';
+
+export default function Top() {
+    return (
+        <div>
+            <div style={{ display: 'flex', paddingTop: 20 }}>
+                <div style={{ flex: '100px 0 0' }}>
+                    <img src="/images/myBubble.png" alt="logo" style={{display: 'block', width: 80}} />
+                </div>
+                <Header as="h1">로코</Header>
+            </div>
+            <Gnb />
+        </div>
+    )
+}
